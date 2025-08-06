bddb42a851bcda42f9f3f8aabf5b5a68da3bd449
ChaitanyaPothuraju <163710102+ChaitanyaPothuraju@users.noreply.github.com>
2025-08-06 19:36:21 +0530

    Update vite.config.js
    Signed-off-by: ChaitanyaPothuraju <163710102+ChaitanyaPothuraju@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/skill3/',  // 👈 important!
   plugins: [react()],
-  base: '/skill3/'
 });
